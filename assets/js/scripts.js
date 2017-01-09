c4c41e2f0378efa3668d1caf75c14e9c43cdf247
L Daniel Swakman <d.swakman@gmail.com>
2017-01-09 21:34:23 +0300

    some content & tweaks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,102 +1,165 @@
 $(document).ready(function() {
-    
-    
-    // scripts voor smooth in-page anchor scrolling
-    $('a[href^=#]').click(function(e) {
-        e.preventDefault();
-        var $target = $(this).attr('href');
-        var off = 100;
-        if ($target === '#top') {
-            $target = '#wrapper';
-            off = 0;
-        }
-        console.log(off);
-        $($target).ScrollTo({
-            duration: 500,
-            animation: 'ease',
-            offsetTop: off,
-            callback: function(){ /*location.hash = $target;*/ }
-        });
+  
+  
+  // scripts for smooth in-page anchor scrolling
+  $('a[href^=#]').click(function(e) {
+    e.preventDefault();
+    var $target = $(this).attr('href');
+    var off = 100;
+    if ($target === '#top') {
+      $target = '#wrapper';
+      off = 0;
+    }
+    console.log(off);
+    $($target).ScrollTo({
+      duration: 500,
+      animation: 'ease',
+      offsetTop: off,
+      callback: function(){ /*location.hash = $target;*/ }
     });
-    
-    
-    // menu hover script
-    $('ul#menu a').hover(function() {
-        hoveredItem = $(this).text();
-        if ($('ul#'+hoveredItem).css('display') == 'none') {
-            $('ul.submenu').css('height','0').hide();
-            
-            var curHeight = $('ul#'+hoveredItem).height();
-            $('ul#'+hoveredItem).show().css('height','auto');
-            var autoHeight = $('ul#'+hoveredItem).height();
-            $('ul#'+hoveredItem).height(curHeight).animate({height: autoHeight}, 200);
-        }
-    });
-    
-    $('ul#menu a.nest').click(function(e) {
-        e.preventDefault();
-        hoveredItem = $(this).text();
-        $('nav').hover(function() {
-        }, function() {
-            if ($('ul#'+hoveredItem).css('display') != 'none') {
-                hovering = setTimeout(function() {$('ul.submenu').stop(true).fadeOut(100)}, 300);
-            }
-        });
-    });
-    
-    var navHeight = $('nav').height();
-    $('.menu-static').css('height',navHeight);
-    
-    var hovering;
+  });
+  
+  
+  // menu hover script
+  $('ul#menu a').hover(function() {
+    hoveredItem = $(this).text();
+    if ($('ul#'+hoveredItem).css('display') == 'none') {
+      $('ul.submenu').css('height','0').hide();
+      
+      var curHeight = $('ul#'+hoveredItem).height();
+      $('ul#'+hoveredItem).show().css('height','auto');
+      var autoHeight = $('ul#'+hoveredItem).height();
+      $('ul#'+hoveredItem).height(curHeight).animate({height: autoHeight}, 200);
+    }
+  });
+  
+  $('ul#menu a.nest').click(function(e) {
+    e.preventDefault();
+    hoveredItem = $(this).text();
     $('nav').hover(function() {
-        clearTimeout(hovering);
     }, function() {
+      if ($('ul#'+hoveredItem).css('display') != 'none') {
         hovering = setTimeout(function() {$('ul.submenu').stop(true).fadeOut(100)}, 300);
+      }
     });
+  });
+  
+  var navHeight = $('nav').height();
+  $('.menu-static').css('height',navHeight);
+  
+  var hovering;
+  $('nav').hover(function() {
+    clearTimeout(hovering);
+  }, function() {
+    hovering = setTimeout(function() {$('ul.submenu').stop(true).fadeOut(100)}, 300);
+  });
+  
+  $(window).scroll(function() {
+    scroll = $(window).scrollTop();
+    navY = $('.menu-static').offset().top;
+    asideY = $('aside').offset().top;
     
-    $(window).scroll(function() {
-        scroll = $(window).scrollTop();
-        navY = $('.menu-static').offset().top;
-        asideY = $('aside').offset().top;
+    if (scroll >= navY) {
+      $('ul.submenu').each(function() {
+        mainMenuItem = $(this).attr('id');
+        mainMenuItemLeft = $('#menu li#'+mainMenuItem).offset().left - $('#menu').offset().left;
+        $(this).css('left',mainMenuItemLeft);
+      });
+      
+      $('nav').addClass('floating');
+      
+      asideX = $('aside').offset().left;
+      $('aside').css('position','fixed');
+      $('aside').css('top','135px');
+      $('aside').css('left',asideX);
+      
+      $('.small_logo').show();
         
-        if (scroll >= navY) {
-            $('ul.submenu').each(function() {
-                mainMenuItem = $(this).attr('id');
-                mainMenuItemLeft = $('#menu li#'+mainMenuItem).offset().left - $('#menu').offset().left;
-                $(this).css('left',mainMenuItemLeft);
-            });
-            
-            $('nav').addClass('floating');
-            
-            asideX = $('aside').offset().left;
-            $('aside').css('position','fixed');
-            $('aside').css('top','135px');
-            $('aside').css('left',asideX);
-            
-            $('.small_logo').show();
-                
-        } else {
-            $('ul.submenu').each(function() {
-                mainMenuItem = $(this).attr('id');
-                mainMenuItemLeft = $('#menu li#'+mainMenuItem).offset().left - $('#menu').offset().left;
-                $(this).css('left',mainMenuItemLeft);
-            });
-            
-            $('nav').removeClass('floating');
-            
-            $('aside').css('position','absolute');
-            $('aside').css('top','120px');
-            $('aside').css('left','auto');
-            
-            $('.small_logo').hide();
-        }
-        
-        if (scroll >= (navY+100) && scroll < (navY+300)) {
-            logoPosY = scroll-navY-300;
-            $('.small_logo').css('top',logoPosY);
-        } else if (scroll > (navY+300)) {
-            $('.small_logo').css('top','0px');
-        }
-    });
+    } else {
+      $('ul.submenu').each(function() {
+        mainMenuItem = $(this).attr('id');
+        mainMenuItemLeft = $('#menu li#'+mainMenuItem).offset().left - $('#menu').offset().left;
+        $(this).css('left',mainMenuItemLeft);
+      });
+      
+      $('nav').removeClass('floating');
+      
+      $('aside').css('position','absolute');
+      $('aside').css('top','120px');
+      $('aside').css('left','auto');
+      
+      $('.small_logo').hide();
+    }
     
+    if (scroll >= (navY+100) && scroll < (navY+300)) {
+      logoPosY = scroll-navY-300;
+      $('.small_logo').css('top',logoPosY);
+    } else if (scroll > (navY+300)) {
+      $('.small_logo').css('top','0px');
+    }
+  });
+  
+  alignMenuItems();
+  
 });
+
+  
+$(window).resize(function() {
+    alignMenuItems();
+});
+
+
+
+function alignMenuItems() {
+  $('ul#menu a').each(function() {
+    if ($(this).text() === '') {
+      $(this).addClass('active');
+      pointerPos = $(this).offset().left-$('nav').offset().left + ($(this).width())/2;
+      $('.pointer').css('left',pointerPos);
+    }
+  });
+  
+  $('ul.submenu').each(function() {
+    mainMenuItem = $(this).attr('id');
+    mainMenuItemLeft = $('#menu li#'+mainMenuItem).offset().left - $('#menu').offset().left;
+    $(this).css('left',mainMenuItemLeft);
+    $('li', this).each(function() {
+      if ($(this).text() === "<?php echo $page['page_title'] ?>") {
+        $('a',this).addClass('active');
+      }
+    });
+  });
+  
+  $('.magnific-popup-image').magnificPopup({
+    type:'image',
+    mainClass: 'mfp-fade',
+    gallery: {
+      enabled: true, // set to true to enable gallery
+    
+      preload: [0,2], // read about this option in next Lazy-loading section
+    
+      navigateByImgClick: true,
+    
+      arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+    
+      tPrev: 'Vorige', // title for left button
+      tNext: 'Volgende', // title for right button
+      tCounter: '<span class="mfp-counter">%curr% van %total%</span>' // markup of counter
+    }, 
+    zoom: {
+      enabled: true, // By default it's false, so don't forget to enable it
+  
+      duration: 300, // duration of the effect, in milliseconds
+      easing: 'ease-in-out', // CSS transition easing function 
+  
+      // The "opener" function should return the element from which popup will be zoomed in
+      // and to which popup will be scaled down
+      // By defailt it looks for an image tag:
+      opener: function(openerElement) {
+        // openerElement is the element on which popup was initialized, in this case its <a> tag
+        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+      }
+    }
+  });
+}
--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -1,17 +1,27 @@
 <nav>
 
+  <? // Main menu ?>
   <ul id="menu">
 
     <? $categories = $site->pages()->visible()->filterBy('template', '!=', 'default')->pluck('template', ',', true) ?>
     <? foreach($categories as $category) : ?>
-      <li id="<?= $category ?>"><a href="#" class="nest"><?= $category ?></a></li>
+      <li id="<?= $category ?>"><a href="#" class="nest <? e($page->template() == $category, ' active') ?>"><?= $category ?></a></li>
     <? endforeach ?>
 
       <? $unnested_pages = $site->pages()->visible()->filterBy('template', 'default') ?>
       <? foreach($unnested_pages as $p) : ?>
-        <li id="<?= $category ?>"><a href="<?= $p->url() ?>" class=""><?= $p->title() ?></a></li>
+        <li id="<?= $category ?>"><a href="<?= $p->url() ?>"<? e($p->isOpen(), ' class="active"'); ?>><?= $p->title() ?></a></li>
       <? endforeach ?>
   </ul>
+
+  <? // Submenus ?>
+  <? foreach($categories as $category) : ?>
+    <ul class="submenu" id="<?= $category ?>">
+      <? foreach($site->pages()->visible()->filterBy('template', $category) as $p) : ?>
+        <li><a href="<?= $p->url() ?>"<? e($p->isOpen(), ' class="active"'); ?>><?= $p->title()->html() ?></a></li>
+      <? endforeach ?>
+    </ul>
+  <? endforeach ?>
 
   <a href="#top" class="small_logo"></a>
 

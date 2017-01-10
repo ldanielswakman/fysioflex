b48256d1a43d1c9840a9ee01b4bf14aaf2c0773f
L Daniel Swakman <d.swakman@gmail.com>
2017-01-10 11:55:30 +0300

    added footer menu

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,36 @@
       <footer>
 
+        <div class="footer_menu">
+
+          <? $categories = $site->pages()->visible()->filterBy('template', '!=', 'default')->pluck('template', ',', true) ?>
+          <? $extrapages = $site->pages()->visible()->filterBy('template', 'not in', $categories); ?>
+
+          <ul id="footermenu">
+            <? foreach($categories as $category) : ?>
+              <li id="<?= $category ?>"><?= $category ?></li>
+            <? endforeach ?>
+            <? if($extrapages->count() > 0): ?>
+              <li id="overig">overig</li>
+            <? endif ?>
+          </ul>
+
+          <? foreach($categories as $category) : ?>
+            <ul class="footersubmenu">
+              <? foreach($site->pages()->visible()->filterBy('template', $category) as $p) : ?>
+                <li><a href="<?= $p->url() ?>"<? e($p->isOpen(), ' class="active"'); ?>><?= $p->title()->html() ?></a></li>
+              <? endforeach ?>
+            </ul>
+          <? endforeach ?>
+          <? if($extrapages->count() > 0): ?>
+            <ul class="footersubmenu overigmenu">
+              <? foreach($extrapages as $p) : ?>
+                <li><a href="<?= $p->url() ?>"<? e($p->isOpen(), ' class="active"'); ?>><?= $p->title()->html() ?></a></li>
+              <? endforeach ?>
+            </ul>
+          <? endif ?>
+
+        </div>
+
         <div class="info">
           <span>&copy; <?= $site->title()->html() ?> <?= date('Y') ?></span>
           <span><?= $site->address()->html() ?></span>

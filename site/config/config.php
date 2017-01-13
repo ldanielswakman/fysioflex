9146e29f4b9975614deb298c9a6691d65729e4d1
L Daniel Swakman <d.swakman@gmail.com>
2017-01-13 14:39:12 +0100

    re-distribute widgets

--- site/config/config.php
+++ site/config/config.php
@@ -30,3 +30,17 @@ of the system, please check out http://getkirby.com/docs/advanced/options
 */
 
 c::set('debug', true);
+
+/*
+
+---------------------------------------
+Widgets (on Panel Dashboard)
+---------------------------------------
+
+*/
+c::set('panel.widgets', array(
+  'account'  => true,
+  'history'  => true,
+  'pages'    => true,
+  'site'     => false,
+));

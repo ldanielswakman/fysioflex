68f8e1521d3721d9e42aec0e70001a745927c49a
L Daniel Swakman <d.swakman@gmail.com>
2017-01-23 11:04:19 +0300

    filter panel pages by template

--- site/widgets/pages2/pages2.html.php
+++ site/widgets/pages2/pages2.html.php
@@ -1,5 +1,10 @@
+<? $categories = $pages->pluck('template', ',', true) ?>
+<? foreach($categories as $category) : ?>
+<h3 style="color: #999;"><?= ucfirst($category) ?></h3>
 <ul class="nav nav-list sidebar-list">
-  <?php foreach($pages as $c): ?>
-  <?php echo new Kirby\Panel\Snippet('pages/sidebar/subpage', array('subpage' => $c)) ?>
+  <? foreach($pages->filterBy('template', $category) as $p) : ?>
+  <?php echo new Kirby\Panel\Snippet('pages/sidebar/subpage', array('subpage' => $p)) ?>
   <?php endforeach ?>
 </ul>
+<br>
+<? endforeach ?>
